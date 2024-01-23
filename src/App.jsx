f4e9a8fd1602c7d37b0b4be335c30772cad7aacf
kaiumuddin <kaium.uddin2909@gmail.com>
2024-01-24 04:22:42 +0600

    Movie and all its functionality done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import MovieList from "./cine/MovieList";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
@@ -8,8 +10,10 @@ export default function App() {
             <main>
                 <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
                     <Sidebar />
+                    <MovieList />
                 </div>
             </main>
+            <Footer />
         </>
     );
 }
